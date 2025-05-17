bfd2542450157db6f09f44313cc58356cb337e43
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-05-17 19:45:35 +0530

    Added: show diagnostic messages

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -79,3 +79,12 @@ vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default
 vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
+
+-- Show diagnostics messages
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
