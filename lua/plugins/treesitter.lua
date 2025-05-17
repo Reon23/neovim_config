8abd5d9ac0d941c9b84002f7a071974bcaf22da6
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-05-17 15:03:01 +0530

    Modified: treesiter plugin config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,25 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup {
-      ensure_installed = { "lua", "vim", "bash", "python", "javascript" }, -- add more as needed
+      ensure_installed = { 
+          "lua", 
+          "luadoc", 
+          "vim",
+          "html", 
+          "bash", 
+          "c", 
+          "python", 
+          "javascript",
+          "java",
+          "json",
+          "css",
+          "sql",
+          "dockerfile",
+          "cmake",
+          "make",
+          "yaml"
+      }, -- add more as needed
+      auto_install = true,
       highlight = {
         enable = true,
       },
