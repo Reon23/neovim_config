af9a0da0c3feb771e47bf52cf8b2c1bd12be3559
Reon23 <166202651+Reon23@users.noreply.github.com>
2026-03-30 23:16:54 +0530

    added: keymap

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,16 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+	'stevearc/oil.nvim',
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	-- Optional dependencies
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+
+	config = function(opts)
+		require("oil").setup(opts)
+		vim.keymap.set("n", "<leader>e", ":Oil<CR>", { silent = true, desc = "Open Oil" })
+	end,
 }
