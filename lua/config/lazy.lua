6e159b3d676a560d1430c529916033d0b3928d63
Reon23 <166202651+Reon23@users.noreply.github.com>
2026-03-31 22:37:44 +0530

    info: moved plugins to config folder

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,11 +25,17 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    { import = "config.plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { 
+	  enabled = true,
+	  notify = false
+  },
+  change_detection = {
+	  notify = false
+  }
 })
---
+++ lua/config/plugins/oil.lua
@@ -0,0 +1,23 @@
+return {
+	'stevearc/oil.nvim',
+	---@module 'oil'
+	---@type oil.SetupOpts
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	lazy = false,
+
+	opts = {
+		keymaps = {
+			["<C-s>"] = false,
+			["<C-q>"] = { "actions.close", mode = "n" },
+		},
+
+		view_options = {
+			show_hidden = true
+		}
+	},
+
+	config = function(_, opts)
+		require("oil").setup(opts)
+		vim.keymap.set("n", "<leader>e", ":Oil<CR>", { silent = true, desc = "Open File Explorer" })
+	end,
+}
