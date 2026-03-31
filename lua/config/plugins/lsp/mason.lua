e212350e8784ce5f3651dc733e888e82b7139374
Reon23 <166202651+Reon23@users.noreply.github.com>
2026-03-31 23:36:35 +0530

    feat: lsp

---
+++ lua/config/plugins/lsp/mason.lua
@@ -0,0 +1,22 @@
+return {
+	"mason-org/mason-lspconfig.nvim",
+	opts = {
+		ensure_intalled = {
+			"html",
+			"ts_ls",
+			"cssls",
+			"clangd",
+			"lua_ls",
+			"emmet_ls",
+			"tailwindcss",
+		},
+	},
+	dependencies = {
+		{
+			"mason-org/mason.nvim",
+			opts = {}
+		},
+
+		"neovim/nvim-lspconfig"
+	},
+}	
