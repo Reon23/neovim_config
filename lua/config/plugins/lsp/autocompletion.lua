ec43bd017beeaf5b5d312dede76c47e7699c790d
Reon23 <166202651+Reon23@users.noreply.github.com>
2026-04-01 23:35:39 +0530

    feat: autocompletion

--- lua/config/plugins/lsp/autocompletion.lua
+++ lua/config/plugins/lsp/autocompletion.lua
@@ -1,19 +1,26 @@
 return {
-  "hrsh7th/cmp-nvim-lsp",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    { "antosha417/nvim-lsp-file-operations", config = true },
-    { "folke/lazydev.nvim", opts = {} },
-  },
-  config = function()
-    -- import cmp-nvim-lsp plugin
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	"hrsh7th/cmp-nvim-lsp",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+		{ "folke/lazydev.nvim", opts = {} },
+	},
+	config = function()
+		-- import cmp-nvim-lsp plugin
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-    -- used to enable autocompletion (assign to every lsp server config)
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+		-- used to enable autocompletion (assign to every lsp server config)
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    vim.lsp.config("*", {
-      capabilities = capabilities,
-    })
-  end,
+		vim.lsp.config("*", {
+			capabilities = capabilities,
+		})
+
+		vim.diagnostic.config({
+			virtual_text = true,   -- shows error inline
+			signs = true,          -- gutter icons
+			underline = true,
+			update_in_insert = false,
+		})
+	end,
 }
