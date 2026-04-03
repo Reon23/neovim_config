f4963b1c6761613c73d82694410c38be01e473d8
Reon23 <166202651+Reon23@users.noreply.github.com>
2026-04-03 22:00:59 +0530

    feat: misc

---
+++ lua/config/plugins/misc/init.lua
@@ -0,0 +1,6 @@
+return {
+	require("config.plugins.misc.autopairs"),
+	require("config.plugins.misc.nvim_colorizer"),
+	require("config.plugins.misc.todo_comments"),
+	require("config.plugins.misc.vim_sleuth"),
+}
