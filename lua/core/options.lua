e185c8be6816f396a00f0853743dd49db1e977c7
Reon23 <166202651+Reon23@users.noreply.github.com>
2026-04-03 22:01:16 +0530

    info: add options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,9 +2,22 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.wrap = false
 
+vim.opt.termguicolors = true
+
 vim.opt.clipboard = "unnamedplus"
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
 vim.wo.signcolumn = "yes"
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.cursorline = true
+
+vim.o.swapfile = false
+
+vim.o.smartindent = true
+
+vim.o.undofile = true
