ee8ff6a122fedce4c310c78266891519204263e0
Reon23 <166202651+Reon23@users.noreply.github.com>
2026-05-02 22:27:28 +0530

    info: updated colorscheme

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,31 +1,3 @@
--- return {
--- 	"xero/miasma.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		vim.cmd("colorscheme miasma")
---
--- 		vim.cmd([[
--- 		highlight Normal guibg=none ctermbg=none
--- 		highlight NormalNC guibg=none
--- 		highlight NonText guibg=none
--- 		highlight LineNr guibg=none
--- 		]])
---
--- 	end,
--- }
-
--- return {
---   "ellisonleao/gruvbox.nvim",
---   priority = 1000,
---   config = function(_, opts)
---     require("gruvbox").setup {
---       transparent_mode = true,
---     }
---     vim.cmd "colorscheme gruvbox"
---   end,
--- }
-
 return {
   "kdheepak/monochrome.nvim",
   config = function()
